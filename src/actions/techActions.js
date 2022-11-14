0b436eabe17bb804add41afa1d8afdc658421f66
DLee1993 <Dai180293@gmail.com>
2022-11-14 22:59:23 +0000

    added functinality to add a new technician to the server

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -26,6 +26,37 @@ export const getTechs = () => {
     };
 };
 
+//info - Add a new tech to server
+export const addTech = (tech) => {
+    //info - redux-thunk allows you to return a function that allows us to dispatch to the reducer at any time
+    return async (dispatch) => {
+        try {
+            //info - sets loading to true in the state
+            setLoading();
+
+            //info - make a fetch request to logs to retrieve our data
+            const res = await fetch("/techs", {
+                method: "POST",
+                body: JSON.stringify(tech),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            const data = await res.json();
+
+            //info - dispatch the type which is to get the logs and the payload is the data from the fetch request
+            dispatch({
+                type: ADD_TECH,
+                payload: data,
+            });
+        } catch (error) {
+            dispatch({
+                type: TECHS_ERROR,
+                payload: error.response.data,
+            });
+        }
+    };
+};
 
 //info - Set loading to true
 export const setLoading = () => {
--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 import M from "materialize-css/dist/js/materialize.min.js";
+import PropTypes from "prop-types";
+import { addTech } from "../../actions/techActions";
 
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setlastName] = useState("");
 
@@ -9,6 +12,11 @@ const AddTechModal = () => {
         if (firstName === "" || lastName === "") {
             return M.toast({ html: "Please enter your name" });
         }
+        addTech({
+            firstName,
+            lastName,
+        });
+        M.toast({ html: `Added ${firstName} ${lastName} as a Technician` });
         //info - Clear fields
         setFirstName("");
         setlastName("");
@@ -17,7 +25,7 @@ const AddTechModal = () => {
     return (
         <div id='add-tech-modal' className='modal' style={modalStyle}>
             <div className='modal-content'>
-                <h4 style={{marginBottom: '50px'}}>New Technician</h4>
+                <h4 style={{ marginBottom: "50px" }}>New Technician</h4>
                 <div className='row'>
                     <div className='input-field'>
                         <input
@@ -63,4 +71,8 @@ const modalStyle = {
     height: "75%",
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+    addTech: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addTech })(AddTechModal);
